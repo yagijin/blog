b017017a0021f6a5156d9750f0f65979f5bd2bde
yagijin <mukailab@mukailabnoMacBook-Pro.local>
2020-10-06 04:05:55 +0900

    棋譜機能の追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,6 +52,7 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
+          "gatsby-remark-component",
           {
             resolve: 'gatsby-remark-relative-images',
             options: {
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import rehypeReact from "rehype-react"
+import Kifu from "../components/Kifu"
+
+const renderAst = new rehypeReact({
+  createElement: React.createElement,
+  components: { "kifu": Kifu }
+}).Compiler
 
 export const HTMLContent = ({ content, className }) => (
-  <div className={className} dangerouslySetInnerHTML={{ __html: content }} />
+  /*<div className={className} dangerouslySetInnerHTML={{ __html: content }} />*/
+  <div >{renderAst(content)}</div>
 )
 
 const Content = ({ content, className }) => (
